789dcb72494f0db9c1450dbe4b4ae0b38a733228
rofl0r <rofl0r@users.noreply.github.com>
2021-03-23 05:47:06 +0000

    md-amd64-gcc: remove linux-specific rtc hack
    the timer hack using procfs caused a lot of stutter in the audio
    playback, and just using rdtsc insn seems to work well enough.

--- src/md-x86-gcc/rpt.h
+++ src/md-x86-gcc/rpt.h
@@ -26,34 +26,6 @@ STATIC_INLINE uae_s64 read_processor_time (void)
 	__asm__ __volatile__ (".byte 0x0f,0x31" : "=a" (foo1), "=d" (foo2) :);
 	tsc = (((uae_u64) foo2) << 32ULL) | (uae_u64) foo1;
 
-#ifdef __linux__
-	/* Hack to synchronize syncbase and re-compute
-	 * vsynctime when TSC frequency changes */
-
-/* How many times per second tsc will be synced */
-#define TSC_SYNC_FREQUENCY 8
-	extern frame_time_t linux_get_tsc_freq (void);
-	extern void         compute_vsynctime (void);
-//	extern frame_time_t syncbaseo;
-	extern int syncbase;
-
-	static frame_time_t next_tsc_synctime;
-	static frame_time_t prev_syncbase;
-
-	if (tsc > next_tsc_synctime) {
-		uae_s64 new_tsc_freq = linux_get_tsc_freq ();
-
-		if (new_tsc_freq > 0) {
-			syncbase = new_tsc_freq;
-			next_tsc_synctime = tsc + (syncbase / TSC_SYNC_FREQUENCY);
-
-			if (syncbase != prev_syncbase) {
-				prev_syncbase = syncbase;
-				compute_vsynctime ();
-			}
-		}
-	}
-#endif
 	return tsc;
 }
 
